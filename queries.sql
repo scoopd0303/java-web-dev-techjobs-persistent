8794c899b23c60562994eaf2b9b34d345d9380d5
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-07-08 20:02:58 -0500

    finished assignment.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 ## Part 1: Test it with SQL
 SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'job';
+id int (primary key), employer_name VARCHAR, employer_id int,
 
 ## Part 2: Test it with SQL
 SELECT name FROM employer WHERE location="St. Louis"; -- NOTE: only able to do this from the employer table at this point in the project.
@@ -12,3 +13,8 @@ SELECT DISTINCT skill.name, skill.description FROM job
 	JOIN job_skills ON job.id = job_skills.jobs_id
 		JOIN skill ON skill.id = job_skills.skills_id
 			ORDER BY name ASC;
+
+SELECT name, description FROM skill
+    LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+    WHERE job_skills.jobs_id IS NOT NULL
+    ORDER BY skill.name ASC;
